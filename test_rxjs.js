5d2698e6484c9e0e159b7d76f75040efb5acc136
Huang Jiaqing <657593825@qq.com>
2018-08-16 15:00:06 +0800

    fix: rx

--- test_rxjs.js
+++ test_rxjs.js
@@ -1,9 +1,21 @@
 const Rx = require('rxjs');
 
-function onSubscribe(observer) {
-  observer.next(1)
-}
+// function onSubscribe(observer) {
+//   observer.next(1)
+// }
+//
+// const observable$ = new Rx.Observable(onSubscribe);
+//
+// observable$.subscribe(v => console.log(v));
 
-const observable$ = new Rx.Observable(onSubscribe);
+// or
+
+const observable$ = Rx.Observable.create(function subscribe(observer) {
+  observer.next(1)
+
+  setTimeout(() => {
+    observer.next(2)
+  }, 1000);
+});
 
 observable$.subscribe(v => console.log(v));
